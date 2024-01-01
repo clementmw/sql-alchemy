215a857cf10dbd4cdf5f4ea0460d6a47c632e836
Clement Macharia <clementmacharia62@gmail.com>
2024-01-02 01:33:21 +0300

    added relationship

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine,desc
+from sqlalchemy import create_engine,desc,ForeignKey
 from sqlalchemy import (Column,DateTime,Integer,String)
+from sqlalchemy.orm import (sessionmaker,relationship)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -13,6 +14,9 @@ class Restaurant(Base):
     name = Column(String())
     price = Column(Integer())
 
+    #to establish a connection to reviews
+    reviews  = relationship('Review',backref = 'restaurant')
+
 def __repr__(self):
         return f"restaurant  {self.id}: " \
             + f"{self.name}, " \
@@ -25,8 +29,27 @@ class Customer(Base):
       first_name = Column(String())
       last_name = Column(String())
 
+      reviews = relationship('Review',backref = 'customer')
+
+    
+
 def __repr__(self):
         return f"customer {self.id}: " \
             + f"first name {self.first_name}, " \
             + f" last name {self.last_name}"
 
+
+class Review (Base):
+       __tablename__ = 'reviews'
+
+       id = Column(Integer(), primary_key = True)
+       star_rating = Column(Integer())
+
+       #establish relationship to both restaurant and customer
+       restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+       customer_id = Column(Integer(), ForeignKey('customers.id'))
+
+def __repr__(self):
+        return f"review {self.id}: " \
+            + f"{self.star_rating}, " 
+           
