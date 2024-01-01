e2e87f5a664c4eba9c5af2fc9283740385084028
Clement Macharia <clementmacharia62@gmail.com>
2024-01-02 00:32:55 +0300

    added models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,32 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,desc
+from sqlalchemy import (Column,DateTime,Integer,String)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 engine  = create_engine('sqlite:///hotel.db')
 
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer(), primary_key = True),
+    name = Column(String()),
+    price = Column(Integer())
+
+def __repr__(self):
+        return f"restaurant  {self.id}: " \
+            + f"{self.name}, " \
+            + f"{self.price}"
+
+class Customer(Base):
+      __tablename__ = 'customers'
+
+      id  = Column(Integer(), primary_key= True),
+      first_name = Column(String()),
+      last_name = Column(String())
+
+def __repr__(self):
+        return f"customer {self.id}: " \
+            + f"first name {self.first_name}, " \
+            + f" last name {self.last_name}"
 
