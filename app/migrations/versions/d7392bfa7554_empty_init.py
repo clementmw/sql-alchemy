560b4b1939d47a1788bd1b7ee789ca67678fbddb
Clement Macharia <clementmacharia62@gmail.com>
2024-01-02 00:15:36 +0300

    added alembic

---
+++ app/migrations/versions/d7392bfa7554_empty_init.py
@@ -0,0 +1,24 @@
+"""empty init
+
+Revision ID: d7392bfa7554
+Revises: 
+Create Date: 2024-01-02 00:15:03.904590
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd7392bfa7554'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
